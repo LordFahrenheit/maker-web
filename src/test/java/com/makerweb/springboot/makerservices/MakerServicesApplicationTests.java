1f0bdd722a425ded81248852488206c471526806
Z_Shaw <zns932@utulsa.edu>
2018-01-25 23:47:24 -0600

    Adding Main SRC files

---
+++ src/test/java/com/makerweb/springboot/makerservices/MakerServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.makerweb.springboot.makerservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MakerServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
